e34c149caf64e07dc1aee6b582f2e9eb7f821140
21120442 <duydangtr@gmail.com>
2025-04-22 19:10:32 +0700

    shopping_list

---
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _GroceryListState();
+  }
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final item = await Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => const NewItem()));
+
+    if (item == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(item);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(child: Text("No item"));
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder:
+            (context, index) => Dismissible(
+              key: ValueKey(_groceryItems[index].id),
+              onDismissed: (direction) {
+                _removeItem(_groceryItems[index]);
+              },
+              child: ListTile(
+                title: Text(_groceryItems[index].name),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: _groceryItems[index].category.color,
+                ),
+                trailing: Text(_groceryItems[index].quantity.toString()),
+              ),
+            ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your grocery"),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
+      ),
+      body: content,
+    );
+  }
+}
